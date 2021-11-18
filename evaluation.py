05814264b19c4df357c9e98bcaac3bf4900eec8c
hiroki adachi <ha618@mprg.cs.chubu.ac.jp>
2021-11-18 05:41:32 +0000

    [add] add learning rate decay and binary mask

--- evaluation.py
+++ evaluation.py
@@ -27,18 +27,37 @@ from utils import *
 def optimize(model, index, save_dir, x_org, rec_x, th, alpha, lam, max_iters, loss_func):
     rec_path = os.path.join(save_dir, 'reconstructed_images', 'sample_%d' % index)
     opt_path = os.path.join(save_dir, 'optimized_images', 'sample_%d' % index)
+    grad_path = os.path.join(save_dir, 'gradient_images', 'sample_%d' % index)
+    enegy_path = os.path.join(save_dir, 'enegy_images', 'sample_%d' % index)
     os.makedirs(rec_path, exist_ok=True)
     os.makedirs(opt_path, exist_ok=True)
+    os.makedirs(grad_path, exist_ok=True)
+    os.makedirs(enegy_path, exist_ok=True)
     save_image(x_org, index, 0, opt_path)
     
     loss = loss_func(x_org, rec_x, reduction='sum')
     loss.backward()
     grads = x_org.grad.data
     x_t = x_org - alpha*grads*(x_org - rec_x)**2
+    save_image(normalize(grads), index, 0, grad_path)
+    save_image(normalize((x_org - rec_x)**2), index, 0, enegy_path)
+    
+    losses = torch.zeros(max_iters)
+    decay_rate = 0.1
+    minimum = 1e12
     for i in range(max_iters):
         x_t = Variable(x_t.clamp(min=0, max=1), requires_grad=True)
         rec_x = model(x_t).detach()
         rec_loss = loss_func(x_t, rec_x, reduction='sum')
+        losses[i] = rec_loss.item()
+        
+        ## learning rate decay
+        ## If you'd like to use fixed learning rate, you should comment out bellow 4 lines.
+        if minimum >= rec_loss:
+            minimum = min(minimum, rec_loss)
+        else:
+            alpha *= decay_rate
+        
         if rec_loss <= th:    break
         l1 = torch.abs(x_t - x_org).sum()
         loss = rec_loss + lam*l1
@@ -46,15 +65,23 @@ def optimize(model, index, save_dir, x_org, rec_x, th, alpha, lam, max_iters, lo
         grads = x_t.grad.data
         
         energy = grads * (x_t - rec_x)**2
+        ## If you'd like to use binary mask, you comment out above line and then activate bellow three lines, please.
+        #mask_th = torch.flatten((x_t - rec_x)**2, start_dim=1).median(dim=1)[0]
+        #binary_mask = torch.where(torch.pow(x_t - rec_x, 2)>mask_th, 1, 0)
+        #energy = grads * binary_mask
+        
         x_t = x_t - alpha*energy
         save_image(rec_x, index, i+1, rec_path)
         save_image(x_t, index, i+1, opt_path)
+        save_image(normalize(grads), index, i+1, grad_path)
+        save_image(normalize((x_t - rec_x)**2), index, i+1, enegy_path)
     
     make_gif(image_dir_path=opt_path, sample_idx=index)
-    return rec_x
+    return rec_x, losses
     
 def anomaly_detection(model, dataloader, threshold_rec, save_dir, loss_func, args=None):
     pred_list, label_list = [], []
+    total_losses = torch.zeros(len(dataloader.dataset), args.max_iters)
     for index, (x, y, mask) in tqdm(enumerate(dataloader)):
         mask_path = os.path.join(save_dir, 'mask_images')
         os.makedirs(mask_path, exist_ok=True)
@@ -64,10 +91,12 @@ def anomaly_detection(model, dataloader, threshold_rec, save_dir, loss_func, arg
         x.requires_grad_(True)
         reconstructed_x = model(x).detach()
         if args.use_grad:
-            final_x = optimize(model, index, save_dir, x, reconstructed_x, threshold_rec, 
-                               alpha=args.alpha, lam=args.lam, max_iters=args.max_iters, loss_func=loss_func)
+            final_x, losses = optimize(model, index, save_dir, x, reconstructed_x, threshold_rec, 
+                                       alpha=args.alpha, lam=args.lam, max_iters=args.max_iters, loss_func=loss_func)
+            total_losses[index] = losses
         else:
             final_x = reconstructed_x
+            total_losses = None
         
         dssim_map = get_residual_map(final_x, x, x.size(1))
         pred_list.append(dssim_map)
@@ -90,6 +119,9 @@ def anomaly_detection(model, dataloader, threshold_rec, save_dir, loss_func, arg
     with open(save_dir + 'auroc_score.txt', 'a') as f:
         print("Reconstruction threshold :", threshold_rec, file=f)
         print("auroc_score :", auroc_score, "\n", file=f)
+        
+    #if total_losses is not None:
+    #    avg_losses = torch.mean(total_losses, dim=1)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
